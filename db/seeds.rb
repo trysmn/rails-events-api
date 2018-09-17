01816dd1ed94156a1cbec6b2cd79152e2e202701
Trystan Newbury trystan.newbury@gmail.com <trystan.newbury@gmail.com>
2018-09-17 16:32:00 +0100

    move faker gem to test and development environment group in gemfile and write seed file to create 10 events to be returned by our Events API

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,13 @@
 
 puts "Seeding database with 10 events..."
 
-time_now = Time.now.iso8601(3)
+# we create our base time to work with:
+time_now = Time.new(2018, 9, 17, 9, 0, 0, "+01:00")
 
+# we then create 10 events with the same start and end times,but each event is on the next day after the last one:
 10.times do
-  Event.create(start_time: time_now, end_time: time_now + 14, label: Faker::Ancient.god, category: Faker::Color.color_name )
+  Event.create(start_time: time_now.iso8601(0), end_time: (time_now + (60 * 60 * 7)).iso8601(0), label: Faker::Ancient.god, category: Faker::Color.color_name )
+  time_now += 60 * 60 * 24
 end
 
 puts "All done!"
